b553b8e7a578bf920b63c592c6003b5c6e70638e
Daniil Razorenov <daniltmb@gmail.com>
2023-08-18 23:45:29 +0300

    Update database init file

--- .docker/postgres/00-follower-scan-table.sql
+++ .docker/postgres/00-follower-scan-table.sql
@@ -5,4 +5,15 @@ CREATE TABLE follower_scan
     created_at  timestamp   NOT NULL,
 
     PRIMARY KEY (streamer_id, scan_number)
-)
+);
+
+CREATE TABLE follower
+(
+    streamer_id varchar(50)   NOT NULL,
+    user_id     varchar(50)   NOT NULL,
+    scan_number integer       NOT NULL,
+    user_name   varchar(2048) NOT NULL,
+    followed_at timestamp     NOT NULL,
+
+    PRIMARY KEY (streamer_id, user_id)
+);
